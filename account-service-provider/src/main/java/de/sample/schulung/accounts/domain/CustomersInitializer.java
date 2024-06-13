aa1dab4f5db920e002893479762b513ccd835d83
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-06-13 15:38:33 +0200

    Configure application using application.yml and configuration class

--- account-service-provider/src/main/java/de/sample/schulung/accounts/domain/CustomersInitializer.java
+++ account-service-provider/src/main/java/de/sample/schulung/accounts/domain/CustomersInitializer.java
@@ -1,6 +1,10 @@
 package de.sample.schulung.accounts.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,29 +14,38 @@ import java.time.Month;
 import java.util.UUID;
 
 @Component
+@ConfigurationProperties(prefix = "application.customers.initialization")
 @RequiredArgsConstructor
+@Slf4j
 public class CustomersInitializer {
 
   private final CustomersService service;
 
+  @Getter
+  @Setter
+  private boolean enabled;
+
   @EventListener(ContextRefreshedEvent.class)
   public void init() {
-    service.createCustomer(
-      new Customer(
-        null,
-        "Max",
-        LocalDate.of(2010, Month.FEBRUARY, 10),
-        Customer.CustomerState.ACTIVE
-      )
-    );
-    service.createCustomer(
-      new Customer(
-        UUID.randomUUID(),
-        "Julia",
-        LocalDate.of(2011, Month.APRIL, 2),
-        Customer.CustomerState.DISABLED
-      )
-    );
+    if(this.enabled) {
+      log.info("Initializing customers");
+      service.createCustomer(
+        new Customer(
+          null,
+          "Max",
+          LocalDate.of(2010, Month.FEBRUARY, 10),
+          Customer.CustomerState.ACTIVE
+        )
+      );
+      service.createCustomer(
+        new Customer(
+          UUID.randomUUID(),
+          "Julia",
+          LocalDate.of(2011, Month.APRIL, 2),
+          Customer.CustomerState.DISABLED
+        )
+      );
+    }
   }
 
 
--- account-service-provider/src/test/java/de/sample/schulung/accounts/AccountsApiTests.java
+++ account-service-provider/src/test/java/de/sample/schulung/accounts/AccountsApiTests.java
@@ -16,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+//@ActiveProfiles("local")
 class AccountsApiTests {
 
   @Autowired
