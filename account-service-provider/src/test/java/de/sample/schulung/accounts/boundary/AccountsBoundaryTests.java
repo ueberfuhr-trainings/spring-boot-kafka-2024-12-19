ecd34ded401200245313affd9a1b344d6fe72b62
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-06-13 11:31:45 +0200

    Add further tests

--- account-service-provider/src/test/java/de/sample/schulung/accounts/boundary/AccountsBoundaryTests.java
+++ account-service-provider/src/test/java/de/sample/schulung/accounts/boundary/AccountsBoundaryTests.java
@@ -1,8 +1,8 @@
 package de.sample.schulung.accounts.boundary;
 
 import de.sample.schulung.accounts.domain.CustomersService;
+import de.sample.schulung.accounts.domain.NotFoundException;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +10,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Stream;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,25 +25,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class AccountsBoundaryTests {
 
-  @Autowired
-  MockMvc mvc;
-  @MockBean // injiziere Mock (im Controller)
-  CustomersService service;
+    @Autowired
+    MockMvc mvc;
+    @MockBean // injiziere Mock (im Controller)
+    CustomersService service;
 
-  @Test
-  void shouldReturnEmptyArrayWhenNoCustomersExist() throws Exception {
+    @Test
+    void shouldReturnEmptyArrayOnGetCustomersWhenNoCustomersExist() throws Exception {
 
-    Mockito.when(service.getCustomers())
-        .thenReturn(Stream.empty());
+        when(service.getCustomers())
+                .thenReturn(Stream.empty());
+        mvc.perform(
+                        get("/api/v1/customers")
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("[]"));
 
-    mvc.perform(
-      get("/api/v1/customers")
-        .accept(MediaType.APPLICATION_JSON)
-    )
-      .andExpect(status().isOk())
-      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-      .andExpect(content().string("[]"));
+    }
 
-  }
+    @Test
+    void shouldReturn404OnFindByIdIfCustomerNotFound() throws Exception {
+        UUID customerId = UUID.randomUUID();
+        when(service.findCustomerById(customerId))
+                .thenReturn(Optional.empty());
+        mvc.perform(
+                        get("/api/v1/customers/{id}", customerId)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldReturn404OnDeleteIfCustomerNotFound() throws Exception {
+        UUID customerId = UUID.randomUUID();
+
+        doThrow(NotFoundException.class)
+                .when(service)
+                .deleteCustomer(customerId);
+
+        mvc.perform(
+                        delete("/api/v1/customers/{id}", customerId)
+                )
+                .andExpect(status().isNotFound());
+    }
 
 }
