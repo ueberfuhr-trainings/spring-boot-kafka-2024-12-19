9c842660c371ecf6049c1ee60074b5616795664f
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-12-20 11:57:30 +0100

    Introduce Kafka Consumer.

---
+++ statistics-service-provider/src/main/java/de/sample/schulung/statistics/kafka/CustomerEventListener.java
@@ -0,0 +1,55 @@
+package de.sample.schulung.statistics.kafka;
+
+import de.sample.schulung.statistics.domain.Customer;
+import de.sample.schulung.statistics.domain.CustomersService;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomerEventListener {
+
+  private CustomersService customersService;
+
+  @KafkaListener(
+    topics = "customer-events"
+  )
+  public void consume(
+    @Payload CustomerEventRecord record,
+    @Header(KafkaHeaders.RECEIVED_PARTITION) int partition
+  ) {
+    log.info(
+      "Received record: {} {} from partition: {}",
+      record.eventType(),
+      record.uuid(),
+      partition
+    );
+    switch (record.eventType()) {
+      case "created":
+      case "replaced":
+        if ("active".equals(record.customer().state())) {
+          var customer = Customer
+            .builder()
+            .uuid(record.uuid())
+            .dateOfBirth(record.customer().birthdate())
+            .build();
+          // customersService.saveCustomer(customer);
+        } else {
+          // TODO wenn "created" / nicht "active" -> kein DB-Zugriff
+          // customersService.deleteCustomer(record.uuid());
+        }
+        break;
+      case "deleted":
+        // customersService.deleteCustomer(record.uuid());
+        break;
+      default:
+        throw new ValidationException();
+    }
+  }
+
+}
